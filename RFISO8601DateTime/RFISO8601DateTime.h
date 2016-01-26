a141bb2b231e780fb0662433bf47152e5c52faca
Hindrik Bruinsma <de@readefries.nl>
2016-01-26 11:04:24 +0100

    Initial Commit

---
+++ RFISO8601DateTime/RFISO8601DateTime.h
@@ -0,0 +1,19 @@
+//
+//  RFISO8601DateTime.h
+//  RFISO8601DateTime
+//
+//  Created by Hindrik Bruinsma on 26/01/16.
+//  Copyright © 2016 xs4some. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RFISO8601DateTime.
+FOUNDATION_EXPORT double RFISO8601DateTimeVersionNumber;
+
+//! Project version string for RFISO8601DateTime.
+FOUNDATION_EXPORT const unsigned char RFISO8601DateTimeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RFISO8601DateTime/PublicHeader.h>
+
+
---
+++ RFISO8601DateTimeTests/RFISO8601DateTimeTests.swift
@@ -0,0 +1,36 @@
+//
+//  RFISO8601DateTimeTests.swift
+//  RFISO8601DateTimeTests
+//
+//  Created by Hindrik Bruinsma on 26/01/16.
+//  Copyright © 2016 xs4some. All rights reserved.
+//
+
+import XCTest
+@testable import RFISO8601DateTime
+
+class RFISO8601DateTimeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
