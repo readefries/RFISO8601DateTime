851113a396121b983e5a9716cb963cfdcc57e4c7
Ilya Seliverstov <ilya@amudo.com>
2017-10-27 11:44:52 +0200

    migrated to swift4 removed unused code parts

--- RFISO8601DateTime/NSTimeZone+ISO8601.swift
+++ RFISO8601DateTime/NSTimeZone+ISO8601.swift
@@ -2,44 +2,43 @@
 import Foundation
 
 public extension TimeZone {
-  // Time zone designator e.g., Z, +01 or +01:00
-  public static func timeZoneWithString(_ dateString: String) -> TimeZone! {
-    
-    var timeZoneString: String!
-    
-    if let rangeOfTimezone = dateString.range(of: ISO8601Constants.TimeZoneDesignatorRegexp, options: .regularExpression) {
-      timeZoneString = dateString.substring(with: rangeOfTimezone)
-      
-      if timeZoneString == "Z" {
-        return TimeZone(secondsFromGMT: 0)
-      }
-      
-      if let _ = timeZoneString.range(of: ISO8601Constants.TimeZoneDesignatorRegexp, options: .regularExpression) {
-        timeZoneString = timeZoneString.replacingOccurrences(of: ":", with: "")
-        let string = timeZoneString as NSString
+    // Time zone designator e.g., Z, +01 or +01:00
+    public static func timeZoneWithString(_ dateString: String) -> TimeZone? {
         
-        let hoursString = string.substring(with: NSRange(location: 1,length: 2))
-        var minutesString: NSString! = nil
+        var timeZoneString: String
         
-        if string.length == 5 {
-          minutesString = string.substring(with: NSRange(location: 3,length: 2)) as NSString!
+        if let rangeOfTimezone = dateString.range(of: ISO8601Constants.TimeZoneDesignatorRegexp, options: .regularExpression) {
+            timeZoneString = "\(dateString[rangeOfTimezone])"
+            if timeZoneString == "Z" {
+                return TimeZone(secondsFromGMT: 0)
+            }
+            
+            if let _ = timeZoneString.range(of: ISO8601Constants.TimeZoneDesignatorRegexp, options: .regularExpression) {
+                timeZoneString = timeZoneString.replacingOccurrences(of: ":", with: "")
+                let string = timeZoneString as NSString
+                
+                let hoursString = string.substring(with: NSRange(location: 1,length: 2))
+                var minutesString: NSString! = nil
+                
+                if string.length == 5 {
+                    minutesString = string.substring(with: NSRange(location: 3,length: 2)) as NSString!
+                }
+                
+                let formatter = NumberFormatter()
+                
+                let hours = formatter.number(from: hoursString)
+                let minutes = minutesString == nil ? (0) : formatter.number(from: minutesString as String)
+                
+                var seconds = ((hours?.intValue)! * 3600) + ((minutes?.intValue)! * 60)
+                
+                if timeZoneString.characters.index(of: "\u{2d}") != nil {
+                    seconds = seconds * -1
+                }
+                
+                return TimeZone(secondsFromGMT: seconds)
+            }
         }
         
-        let formatter = NumberFormatter()
-        
-        let hours = formatter.number(from: hoursString)
-        let minutes = minutesString == nil ? (0) : formatter.number(from: minutesString as String)
-        
-        var seconds = ((hours?.intValue)! * 3600) + ((minutes?.intValue)! * 60)
-        
-        if timeZoneString.characters.index(of: "\u{2d}") != nil {
-          seconds = seconds * -1
-        }
-        
-        return TimeZone(secondsFromGMT: seconds)
-      }
+        return nil
     }
-    
-    return nil
-  }
 }
